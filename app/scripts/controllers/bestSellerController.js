736b4e3eee32d3c62e8e417bcc2d587c52744a9d
Sarit Mishra <sarit.mishra@gmail.com>
2014-05-09 07:09:41 -0700

    Using promises to populate initial screen

--- app/scripts/controllers/bestSellerController.js
+++ app/scripts/controllers/bestSellerController.js
@@ -1,4 +1,4 @@
-/* global angular */
+/* global angular,console */
 (function () {
     "use strict";
 
@@ -9,10 +9,10 @@
         model.categories = [];
 
         model.getCategories = function() {
-            bestSellerFactory.getCategories()
-                .get(function(results) {
-                    model.categories = results.results;
-                });
+            return bestSellerFactory.getCategories()
+                        .get(function(results) {
+                            model.categories = results.results;
+                        });
         };
 
         model.getDetail = function (book, detail) {
@@ -29,11 +29,17 @@
                .get(function (results) {
                    model.bestSellers = results.results;
                });
+
         };
 
         var init = function() {
-            model.getCategories();
-            // model.getDataFor(model.selectedCategory.list_name); // NOT SURE WHY THIS WONT WORK
+            model.getCategories().$promise //this is calling model.getCategories() first, before handing over the $promise
+                .then(function() {
+                    model.selectedCategory = model.categories[0]; // This will update the drop-down when the page loads.
+                    model.getDataFor(model.selectedCategory.list_name);
+                // },function (reason) {
+                //     console.log("Sorry, failed :" + reason);
+                });
         };
         init();
 
