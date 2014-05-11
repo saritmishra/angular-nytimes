25a231750a90a4c306524bc89ae9cd2bed9b220e
Sarit Mishra <sarit.mishra@gmail.com>
2014-05-10 18:11:30 -0700

    Trying to implement caching in the factory (based on a lastUpdated date). Probably not required because browser (or $resource, not sure which) seems to be doing caching itself.

--- app/scripts/services/bestSellerFactory.js
+++ app/scripts/services/bestSellerFactory.js
@@ -1,4 +1,4 @@
-/* global  angular */
+/* global  angular, console */
 
 (function () {
     "use strict";
@@ -6,13 +6,49 @@
     var bestSellerFactory = function($resource) {
         var factory = {};
 
-        factory.getBestSellers = function(category){
-            return $resource("/getBestSellers/" + category);
+        factory.getBestSellers = function fn(category){
+            if (!fn.cache)
+                fn.cache = {};
+            if (!fn.cache[category])
+                fn.cache[category] = {};
+
+            // var deferred = $q.defer();
+
+            // if this category was never fetched before or if the data is stale...
+            if (!fn.cache[category].lastUpdated || (new Date() - fn.cache[category].lastUpdated.getDate() > 1)) {
+                console.log("fetching from backend...");
+                fn.cache[category].lastUpdated = new Date();
+
+                $resource("/getBestSellers/" + category)
+                .get(function (results){
+                    fn.cache[category].results = results.results;
+                    return  fn.cache[category].results;
+                });
+            } else {
+                console.log("using cached copy");
+                console.log("last updated:" + fn.cache[category].lastUpdated);
+                console.log("time now: " + new Date());
+                return fn.cache[category].results;
+            }
+             // bestSellerFactory.getBestSellers(category)
+             //   .get(function (results) {
+             //       model.bestSellers = results.results;
+             //   });
+
         };
 
         factory.getCategories = function(){
             return $resource("/getCategories");
         };
+
+        // var refreshData = function() {
+        //     var today = new Date();
+        //     var isStale = (today.getDate() - this.lastFetched.getDate() > 1);
+
+        //     if (!this.lastFetched ||  isStale) {
+        //         this.lastFetched = new Date();
+        //       }
+        // };
 
         return factory;
     };
